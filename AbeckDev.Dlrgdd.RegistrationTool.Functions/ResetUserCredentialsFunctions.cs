3e7d9372380713526b3d6688159859b1bc2448db
Alexander Beck <alexander.beck@microsoft.com>
2021-10-06 14:33:44 +0200

    Implemented #7
    Users can now reset their password via self service. It will retrigger the mail send flow.

--- AbeckDev.Dlrgdd.RegistrationTool.Functions/ResetUserCredentialsFunctions.cs
+++ AbeckDev.Dlrgdd.RegistrationTool.Functions/ResetUserCredentialsFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,45 +8,36 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using AbeckDev.Dlrgdd.RegistrationTool.Functions.Services;
-using AbeckDev.Dlrgdd.RegistrationTool.Functions.Models;
 
 namespace AbeckDev.Dlrgdd.RegistrationTool.Functions
 {
-    public static class GetMyEventInformationFunction
+    public static class ResetUserCredentialsFunctions
     {
-        [FunctionName("GetMyEventInformationFunction")]
+        [FunctionName("ResetUserCredentialsFunctions")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            //Get encryption service
+            //Load the encryption module
             var encryptionService = new EncryptionService();
-            //Get attendee service
+            //Get attendee service for table storage communication
             var attendeeService = new AttendeeService();
+            //GetEmailService
+            var emailService = new EmailService();
 
-            //Build request mode
-            string userId;
-            string eMail;
+           
 
             var InputMessage = req.Query;
             //set userId to parse
-            if (InputMessage.ContainsKey("userId"))
-            {
-                userId = InputMessage["userId"];
-            }
-            else
-            {
+            if (!InputMessage.ContainsKey("userId"))
+            {            
                 return new BadRequestObjectResult("Please provide your userId");
             }
 
             //Set mail to parse
-            if (InputMessage.ContainsKey("eMail"))
-            {
-                eMail = InputMessage["eMail"];
-            }
-            else
+            if (!InputMessage.ContainsKey("eMail"))
             {
                 return new BadRequestObjectResult("Please provide your eMail");
             }
@@ -54,7 +45,7 @@ namespace AbeckDev.Dlrgdd.RegistrationTool.Functions
             //UserId and eMail is present 
 
             //Get Record based on id from Table 
-            var attendeeRecord = attendeeService.GetAttendeeRecord(userId);
+            var attendeeRecord = attendeeService.GetAttendeeRecord(InputMessage["userId"]);
             if (attendeeRecord == null)
             {
                 return new NotFoundObjectResult("We could not find a registration based on the userId and eMail combination");
@@ -62,14 +53,16 @@ namespace AbeckDev.Dlrgdd.RegistrationTool.Functions
             attendeeRecord = encryptionService.DecryptAttendeeRecord(attendeeRecord);
 
             //Check if eMail adress is a match with record 
-            if (attendeeRecord.Email != eMail)
+            if (attendeeRecord.Email != InputMessage["eMail"])
             {
                 return new NotFoundObjectResult("We could not find a registration based on the userId and eMail combination");
             }
 
-            //User proved that he can access his record 
-            return new OkObjectResult(new MyEventInformation(attendeeRecord));
+            //User proved that he can access his record
+            await emailService.SendRegistrationSucceededMail(attendeeRecord.Email, attendeeRecord.Name + " " + attendeeRecord.Surname, attendeeRecord.Username, attendeeRecord.Password);
 
+            //Return Ok
+            return new OkResult();
         }
     }
 }
